c4517a8569abab6485d91accf1a39e4c709ed1e1
Syarif Ys <5yarif.y.s@gmail.com>
2022-10-23 09:40:26 +0800

    23 Oktober 2022 video 1 video 2 video 3 video 4 video 5 Video 6 How To Modify The Django Admin Area Video 7 How To Add Database Forms To A Web Page Video 8 How To Query The Database For Venues

--- codemy/events/views.py
+++ codemy/events/views.py
@@ -2,10 +2,19 @@ import calendar
 from django.shortcuts import render
 from calendar import HTMLCalendar
 from datetime import datetime
-from .models import Event
+from .models import Event, Venue
 from .forms import VenueForm
 from django.http import HttpResponseRedirect
 # Create your views here.
+
+def show_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    return render(request, 'events/show_venue.html',
+                  {'venue':venue})
+def list_venues(request):
+    venue_list = Venue.objects.all()
+    return render(request, 'events/venue.html',
+                  {'venue_list':venue_list})
 
 def add_venue(request):
     submitted = False
